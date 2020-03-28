0d900d7279d03e5b21ca96b1c03d7597eb3b1798
kroham2019 <53019429+kroham2019@users.noreply.github.com>
2020-03-28 14:31:34 +0800

    finished react app

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Recipe = ({title,calories,image,ingredients}) => {
 return(
-<div>
+<div className='card'>
     <h1>{title}</h1>
-    <p>Calories: {calories} </p>
     <img src={image} alt={title} />
+    <div className='recipe-box'>
+    <p>Calories: {calories} </p>
     <ul className='ingredients-list'>
         {ingredients.map(ingredient => (
         <li>{ingredient.text}</li>
         ))}
     </ul>
+    </div>
 </div>
 );
 };
