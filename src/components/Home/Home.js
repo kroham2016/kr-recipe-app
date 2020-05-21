d93d1f9ab023cd9a1d7d4d3494b02419aeb8538e
Keon <keon.roham@gmail.com>
2020-05-21 15:52:57 +0800

    fixed homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,11 +5,11 @@ export default function Home() {
     return (
         <div className="jumbotron jumbotron-fluid" style={{"height":"90vh"}}>
             <div className="row">
-                <h1 className='display-4 px-5'>Whats cooking, good lookin?</h1>
+                <h1 className='display-4 px-5'>Whats cookin?</h1>
             </div>
             <div className="row">
                 <div className="col-6">
-                    <p className="lead px-5">Search any keyword in the box above to find delicious recipes to make. Please note the API allows a maximum of 5 searches each minute.</p>
+                    <p className="lead px-5">Search any keyword to find delicious recipes. Note: the API allows maximum of 5 searches/minute.</p>
                 </div>
             </div>
         </div>
